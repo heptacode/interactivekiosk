8ab659af253f235886048b1b08f0ced2b61e76b1
heptacode <admin@hyunwoo.kim>
2020-12-05 14:04:53 +0900

    Update Project

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,26 +2,38 @@ import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
 import Home from "@/views/Home.vue";
 import Order from "@/views/Order.vue";
+import Admin from "@/views/Admin.vue";
+import VoiceOrder from "@/views/VoiceOrder.vue";
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
-	{
-		path: "/",
-		name: "Home",
-		component: Home,
-	},
-	{
-		path: "/order",
-		name: "Order",
-		component: Order,
-	},
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/order",
+    name: "Order",
+    component: Order,
+  },
+  {
+    path: "/voiceorder",
+    name: "VoiceOrder",
+    component: VoiceOrder,
+  },
+  {
+    path: "/admin",
+    name: "Admin",
+    component: Admin,
+  },
 ];
 
 const router = new VueRouter({
-	mode: "history",
-	base: process.env.BASE_URL,
-	routes,
+  mode: process.env.IS_ELECTRON ? "hash" : "history",
+  base: process.env.BASE_URL,
+  routes,
 });
 
 export default router;
---
+++ src/views/Admin.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="page-admin">
+    <header class="path-list">
+      <router-link to="/" class="path">
+        <i class="iconify home" data-icon="mdi-home"></i>
+      </router-link>
+      <span class="path">관리자</span>
+    </header>
+    <div v-for="(item, index) in stock" :key="index">
+      {{ item }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { StockItem } from "../schema";
+
+@Component({})
+export default class Admin extends Vue {
+  stock: StockItem[] = this.$store.state.stock;
+}
+</script>
+
+<style lang="scss" scoped></style>
