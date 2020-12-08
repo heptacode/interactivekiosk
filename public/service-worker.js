7b95d8ab96e076a85eede26affe96e1add027351
heptacode <admin@hyunwoo.kim>
2020-12-09 00:02:31 +0900

    Update serviceWorker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,23 +1,6 @@
 var cacheName = "firebaseStorage";
 var urlsToCache = ["/assets/images/payments.svg", "/assets/images/credit_card.svg"];
 
-self.addEventListener("install", function(event) {
-	event.waitUntil(
-		caches.open(cacheName).then(function(cache) {
-			return cache.addAll(urlsToCache);
-		})
-	);
-});
-
-self.addEventListener("fetch", function(event) {
-	event.respondWith(
-		caches.match(event.request).then(function(response) {
-			if (response) return response;
-			return fetch(event.request);
-		})
-	);
-});
-
 importScripts("/__/firebase/8.1.2/firebase-app.js");
 importScripts("/__/firebase/8.1.2/firebase-messaging.js");
 importScripts("/__/firebase/init.js");
@@ -28,7 +11,7 @@ messaging.onMessage((payload) => {
 });
 
 messaging
-	.getToken({ vapidKey: process.env.VUE_APP_FIREBASE_WEBPUSH_KEY })
+	.getToken({ vapidKey: "BE8_WQxU18fUBK7Q7B3eeoXFJKn_0hDJIZ2E7dT5xZJY6IYYFUz96tv4fL2sJzox8btaFP7b6cH_MtqgJDrPVqE" })
 	.then((currentToken) => {
 		if (currentToken) {
 			sendTokenToServer(currentToken);
@@ -57,4 +40,21 @@ messaging.onBackgroundMessage((payload) => {
 	};
 
 	self.registration.showNotification(notificationTitle, notificationOptions);
+});
+
+self.addEventListener("install", function(event) {
+	event.waitUntil(
+		caches.open(cacheName).then(function(cache) {
+			return cache.addAll(urlsToCache);
+		})
+	);
+});
+
+self.addEventListener("fetch", function(event) {
+	event.respondWith(
+		caches.match(event.request).then(function(response) {
+			if (response) return response;
+			return fetch(event.request);
+		})
+	);
 });
