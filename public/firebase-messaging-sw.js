0fdea5c81b287437424e92149332c50a3685a575
heptacode <admin@hyunwoo.kim>
2020-12-09 07:56:38 +0900

    Update FCM serviceWorker

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -7,9 +7,9 @@ const messaging = firebase.messaging();
 messaging.onBackgroundMessage((payload) => {
 	console.log("[firebase-messaging-sw.js] Received background message ", payload);
 
-	const notificationTitle = "Background Message Title";
+	const notificationTitle = "Interactive Kiosk";
 	const notificationOptions = {
-		body: "Background Message body.",
+		body: "새로운 알림이 있습니다.",
 		icon: "/assets/images/favicon.png",
 	};
 
